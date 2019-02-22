c71feb5be8c7eb093c7409d16e077f06c4acbbe9
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-02-22 16:44:50 -0500

    Entities were created

--- RelationalModel/RelationalModel/DataModel1.Designer.cs
+++ RelationalModel/RelationalModel/DataModel1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\User\Source\Repos\Jamf99\InformationSystemsModel\RelationalModel\RelationalModel\DataModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\User\Source\Repos\Jamf99\InformationSystemsModel\RelationalModel\RelationalModel\DataModel1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
